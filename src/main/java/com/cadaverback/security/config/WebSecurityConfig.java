0f4126d7f8e44d97e254ae336daa13eacb22f3b9
jeangb <jeangb@hotmail.fr>
2022-07-14 21:55:07 +0200

    [feat] WebSecurityConfig : autorisation du service de génération de phrase aléatoire

--- src/main/java/com/cadaverback/security/config/WebSecurityConfig.java
+++ src/main/java/com/cadaverback/security/config/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
         httpSecurity.csrf().disable()
                 // dont authenticate this particular request
                 .authorizeRequests().antMatchers(HttpMethod.POST, "/authenticate", "/register").permitAll().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/api/phrases").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/phrases", "/api/phrases/generaterandom").permitAll()
                 // all other requests need to be authenticated
                 .anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
